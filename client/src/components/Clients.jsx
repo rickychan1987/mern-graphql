4a76a91130ff8d419cca7330e2945ebb3b9ee98a
rickychan1987 <khaivanla1987@gmail.com>
2023-12-20 22:32:08 +0700

    Delete Client Mutation 1h40

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,36 +1,33 @@
 import { gql, useQuery } from "@apollo/client";
-
-const GET_CLIENTS = gql`
-  query getClients {
-    clients {
-      id
-      name
-      email
-      phone
-    }
-  }
-`
+import ClientRow from './ClientRow';
+import Spinner from "./Spinner";
+import {GET_CLIENTS} from "../queries/clientQueries"
 
 const Clients = () => {
   const {loading, error, data} = useQuery(GET_CLIENTS);
 
-  if (loading) return <p>Loading...</p>
+  if (loading) return <Spinner />
   if (error) return <p>Something Went Wrong</p>
 
   return (
     <>
-      {!loading && !error && (
-        <table className="table table-hover mt-3">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th></th>
-            </tr>
-          </thead>
-        </table>
-      )}
+    {!loading && !error && (
+      <table className="table table-hover mt-3">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.clients.map(client => (
+            <ClientRow key={client.id} client={client}/>
+          ))}
+        </tbody>
+      </table>
+    )}
     </>
   )
 }
